19d06bcf3146faa56a98f20e734568dd4a52b859
ibrahim <kanrahim630@gmail.com>
2026-05-08 18:51:54 +0000

    amelioration importante sur le dashboard admin

--- emsp1/asgi.py
+++ emsp1/asgi.py
@@ -14,9 +14,8 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emsp1.settings')
 
-from apps.scolarite.routing import websocket_urlpatterns
-
 django_asgi_app = get_asgi_application()
+from apps.scolarite.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
